1cdbeefadb058c926c82672b134bd2e3a43e5813
Sébastien Dos Santos <sebastdossantos@gmail.com>
2015-10-01 17:50:15 +0100

    START

---
+++ src/Run.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Theo Theodoridis.
+ * Class    : Run
+ * Version  : v1.0
+ * Date     : 06/05/15
+ * User     : ttheod
+ * email    : ttheod@gmail.com
+ * Comments : Class used to launch the interface.
+ **/
+
+public class Run
+{
+   /**
+    * Method     : Run::main()
+    * Purpose    : Default main method used for launch the interface.
+    * Parameters : - args : Initialization parameters.
+    * Returns    : Nothing.
+    * Notes      : None.
+    **/
+    public static void main(String[] args)
+    {
+        new GUI();
+    }
+}
